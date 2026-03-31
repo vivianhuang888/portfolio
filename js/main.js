9f097f9cda7aa67bbeaaba216815498761ed6d13
vivianhuang888 <weian@bu.edu>
2026-03-31 14:16:00 -0400

    Restyle portfolio to closely match Hannah-style multi-page layout

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const toggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+if (toggle && navLinks) {
+  toggle.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+  });
+}
